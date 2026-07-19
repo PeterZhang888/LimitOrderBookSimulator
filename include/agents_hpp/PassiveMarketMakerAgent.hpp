684e3807da7533e34208242d117bc0ecf0b5cfc3
Chaoyang Zhang <pz836740341@gmail.com>
2026-07-19 17:01:41 +0100

    Update PassiveMarketMakerAgent.hpp

--- include/agents_hpp/PassiveMarketMakerAgent.hpp
+++ include/agents_hpp/PassiveMarketMakerAgent.hpp
@@ -1,37 +1,50 @@
 #pragma once
 
-#include "LimitOrderBook.hpp"
+#include "common/AgentUtilities.hpp"
 
 #include <cstdint>
-#include <random>
+#include <vector>
+
+namespace dlob {
+
+struct PassiveMarketMakerConfig {
+    int tick_size = 100;
+    int num_levels = 1;
+    int order_quantity = 100;
+    int level_spacing_ticks = 1;
+    int min_spread_ticks = 2;
+    std::int64_t quote_interval_ns = 20'000'000LL;
+    double quote_skip_probability = 0.0;
+    double recovery_quote_skip_probability = 0.0;
+};
 
 class PassiveMarketMakerAgent {
 public:
-    PassiveMarketMakerAgent(
-        int tick_size,
-        int num_levels,
-        int order_quantity,
-        int level_spacing_ticks,
-        int min_spread_price,
-        double quote_skip_probability,
-        double recovery_quote_skip_probability,
-        std::uint64_t seed
-    );
+    PassiveMarketMakerAgent(int owner_id,
+                            const PassiveMarketMakerConfig& config,
+                            std::int64_t simulation_start_ns,
+                            std::uint64_t seed);
 
-    std::size_t wake_up(LimitOrderBook& book, std::uint64_t& next_order_id, std::int64_t current_time_ns);
+    void generate_orders(const MarketState& current,
+                         std::int64_t window_start_ns,
+                         std::int64_t window_end_ns,
+                         OrderMessageBuilder& builder,
+                         std::vector<OrderMessage>& out);
+
+    void apply_report(const AgentReport& report);
+    int owner_id() const { return owner_id_; }
+    int inventory() const { return inventory_; }
 
 private:
-    int agent_index_ = 0;
-    int tick_size_ = 100;
-    int num_levels_ = 1;
-    int order_quantity_ = 100;
-    int level_spacing_ticks_ = 1;
-    int min_spread_price_ = 200;
-    double quote_skip_probability_ = 0.0;
-    double recovery_quote_skip_probability_ = 0.0;
-    int inventory_ = 0;
-    double cash_ = 0.0;
-    std::mt19937_64 rng_;
+    int sample_quantity(Side side);
+    int inventory_skew_ticks() const;
 
-    static int number_of_agents_;
+    int owner_id_ = 0;
+    PassiveMarketMakerConfig config_{};
+    std::int64_t next_wake_ns_ = 0;
+    FastRng rng_;
+    int inventory_ = 0;
+    double cash_ticks_ = 0.0;
 };
+
+} // namespace dlob
